44c973c1fc6c0314edfbb110ff9e9df566ecaf7f
Steven Frasica <sfrasica1@gmail.com>
2019-12-28 17:06:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def base_hash
-  {Starter: "Pikachu"}# Remember implicit return! We're returning this new Hash _without_ the
+  {railroads: "Pikachu"}# Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 end
 
